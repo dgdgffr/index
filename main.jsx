bc10c8dec75d2fadcf0cfcc688eec206cba84dd1
dgdgffr <wus79849@gmail.com>
2025-04-17 16:51:34 +0700

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => <h2>Nova Lux Capital – 啟動未來資產</h2>;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
